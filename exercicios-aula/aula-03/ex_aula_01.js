307aa3dd4903e989ee4e2fa70be3b73e31a93fd2
Andressa Werner <awkpereira@inf.ufpel.edu.br>
2021-06-09 21:01:35 -0300

    feat/organizacao de pastas e exercicios aula 03

---
+++ exercicios-aula/aula-03/ex_aula_01.js
@@ -0,0 +1,17 @@
+//tarefa (a)
+console.log(42 * -3.14)
+
+//tarefa (b)
+const otterwise = 'Otter' + 'wise'
+console.log(otterwise)
+
+//tarefa (c)
+let and = true && false
+console.log(and)
+
+//tarefa (d)
+let or = true || false
+console.log(or)
+
+//tarefa (e)
+console.log(!true)
