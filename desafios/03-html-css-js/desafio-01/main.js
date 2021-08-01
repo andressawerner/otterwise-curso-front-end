eacc0ffa0e6888fea1c601946683078ec560b939
Andressa Werner <awkpereira@inf.ufpel.edu.br>
2021-08-01 20:13:00 -0300

    feat/insercao-do-post-forms-de-contato-e-ajustes-de-estilo

--- desafios/03-html-css-js/desafio-01/main.js
+++ desafios/03-html-css-js/desafio-01/main.js
@@ -16,4 +16,49 @@ function closeMenuMobile(elem) {
   menuMobile.classList.toggle('active')
 }
 
-window.onload = () => {}
+window.onload = () => {
+  const contactForm = document.querySelector('#contact-form')
+  contactForm.addEventListener('submit', onSubmit)
+}
+
+function onSubmit(event) {
+  event.preventDefault()
+
+  const name = document.querySelector('#name').value
+  const email = document.querySelector('#email').value
+  const phone = document.querySelector('#phone').value
+  const message = document.querySelector('#message').value
+
+  fetch('https://otterwise-fake-api.herokuapp.com/contact', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      phone: phone,
+      message: message,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then(response => response.json())
+    .then(json => {
+      const responseForm = document.querySelector('#response-contact-form')
+      if (json.type === 'Sucesso') {
+        responseForm.innerText =
+          'Recebemos a sua mensagem! Aguarde nosso contato.'
+        const contactForm = document.querySelector('#contact-form')
+        contactForm.reset()
+      } else {
+        responseForm.innerText = `Por favor, verifique as suas informações.`
+        const idError = json.message
+          .replace('O campo ', '')
+          .replace(' não pode estar vazio!!', '')
+        document.querySelector('#' + idError).classList = 'error'
+        setTimeout(
+          () => document.querySelector('#' + idError).removeAttribute('class'),
+          2000
+        )
+      }
+    })
+}
