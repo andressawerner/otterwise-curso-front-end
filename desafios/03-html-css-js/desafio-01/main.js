12caa171fa1f8eef0e8dacf2e4b3c6ce6e963195
Andressa Werner <awkpereira@inf.ufpel.edu.br>
2021-08-01 17:18:28 -0300

    fix/ajustes-de-layout-mobile

--- desafios/03-html-css-js/desafio-01/main.js
+++ desafios/03-html-css-js/desafio-01/main.js
@@ -1,3 +1,19 @@
-function myFunction(x) {
-  x.classList.toggle('change')
+function openMenuMobile(elem) {
+  if (elem.classList.contains('change')) {
+    elem.innerHTML = '<i class="fas fa-bars fa-2x"></i>'
+  } else {
+    elem.innerHTML = '<i class="fas fa-times fa-2x"></i>'
+  }
+  document.getElementById('menu-mobile').classList.toggle('active')
+  elem.classList.toggle('change')
 }
+
+function closeMenuMobile(elem) {
+  const menuMobile = document.getElementById('menu-mobile')
+  const topBarMobile = document.getElementById('topbar-mobile')
+  topBarMobile.innerHTML = '<i class="fas fa-bars fa-2x"></i>'
+  topBarMobile.classList.toggle('change')
+  menuMobile.classList.toggle('active')
+}
+
+window.onload = () => {}
